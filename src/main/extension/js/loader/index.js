4f7641d78469c1268101fff25fcb8d6360b9d7e2
Imre Tabur <imre.tabur@mail.ee>
2024-01-31 23:21:01 +0200

    untested loading and loaded execution

--- src/main/extension/js/loader/index.js
+++ src/main/extension/js/loader/index.js
@@ -1,2 +1,31 @@
 console.log("Loader scripts");
 document.body.style.border = "5px solid red";
+
+const extensionStorage = browser.storage.local;
+const keyName = "firefoxScriptExtensionValue";
+
+function loadValue() {
+    extensionStorage.get(keyName)
+        .then(function (obj) {
+            if (!!obj[keyName]) {
+                let url = obj[keyName].value;
+                // loadScript(url);
+            }
+        }, function (error) {
+            console.error(`Error: ${error}`);
+        });
+}
+
+function loadScript(url) {
+    const script = document.body.createElement('script');
+    script.src = url;
+    script.onload = function () {
+        console.log(`JavaScripti file ${jsFileUrl} loaded.`);
+    };
+    script.onerror = function () {
+        console.error(`Error on ${jsFileUrl} file loading.`);
+    };
+    document.head.appendChild(script);
+}
+
+loadValue();
