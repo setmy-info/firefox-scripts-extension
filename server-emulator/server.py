18413ba50bc8c4e74ca6f638f8cc0b9db05af8c5
Imre Tabur <imre.tabur@mail.ee>
2024-02-01 22:45:45 +0200

    some changes

--- server-emulator/server.py
+++ server-emulator/server.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, send_from_directory, render_template, jsonify
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/', methods=['POST', 'PUT', 'DELETE'])
 def responses():
     return 'Response to {}.'.format(request.method)
 
+
 @app.route('/<path:filename>', methods=['GET'])
 def serve_file(filename):
     return send_from_directory('.', filename)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
